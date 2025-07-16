051f1a351b1f5eb17aa7d7627662cdeef89845f3
Yared <alemayehuyared9@gmail.com>
2025-07-16 12:59:55 -0700

    feat add batch processing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,34 @@
+import mysql.connector
+import os
+import seed
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def stream_users_in_batches(batch_size):
+    try:
+        conn = seed.connect_to_alx_prodev()
+        if conn is None:
+            print("Failed to connect to the database.")
+            return
+        
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data")
+        
+        while True:
+            rows = cursor.fetchmany(batch_size)
+            if not rows:
+                break
+            yield rows
+
+        cursor.close()
+        conn.close()
+    except mysql.connector.Error as err:
+        print(f"Error streaming users in batches: {err}")
+        return  
+    
+def batch_process_users(batch_size):
+    for user in stream_users_in_batches(batch_size):
+        # Process each user here
+        if user["age"] > 25:
+            print(user)
