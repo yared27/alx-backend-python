13801699036acbae344943da22d5a90819a92203
Yared <alemayehuyared9@gmail.com>
2025-07-18 01:31:16 -0700

    feat add query logger decorator

---
+++ python-generators-0x00/0-log_queries.py
@@ -0,0 +1,23 @@
+import seed
+
+def log_queries(func):
+    def wrapper(query, *args, **kwargs):
+        print(f"Executing query: {query}")
+        return func(query, *args, **kwargs)
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    conn = seed.connect_to_alx_prodev()
+    if conn is None:
+        print("Failed to connect to the database.")
+        return []
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute(query)
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results
+
+users = fetch_all_users("SELECT * FROM user_data")
+print(users)
