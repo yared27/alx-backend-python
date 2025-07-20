cfe00dd56aca958f2117aaaa16c9329b39c1dbf6
Yared <alemayehuyared9@gmail.com>
2025-07-20 06:09:22 -0700

    add unittest for access nested map method

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,14 @@
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+class TestAccessNestedMap(unittest.TestCase):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": {"b": {"c": 3}}}, ("a", "b", "c"), 3),
+    ])
+    def test_access_nested_map(self, input_map, path, expected):
+        self.assertEqual(access_nested_map(input_map, path), expected)
+
+    
