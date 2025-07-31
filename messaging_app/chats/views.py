7fc35bff8d365b43420bc3ca035f6fbb04cace52
Yared <alemayehuyared9@gmail.com>
2025-07-31 04:15:53 -0700

    feat: Implement API viewsets for conversations and messages with nested relationships

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from django.shortcuts import get_object_or_404
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
 # Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+    
+    @action(detail=True, methods=['get'], permission_classes=[permissions.IsAuthenticated])
+    def messages(self, request, pk=None):
+        conversation = self.get_object()
+        messages = conversation.messages.all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        conversation_id = self.request.data.get('conversation')
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+        serializer.save(sender=self.request.user, conversation=conversation)
